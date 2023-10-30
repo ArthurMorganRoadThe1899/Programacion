aefed2efa5c89c057d50aacb1145823c9b74dd19
Joselito <josalbarr@alu.edu.gva.es>
2023-10-30 19:05:26 +0100

    Ejercicio 12 acabado

---
+++ EjerciciosDeBuclesJava/B_JAA12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class B_JAA11 {
+    public static void main(String[] args){
+        int A = 0, B = 0, resultado = 1;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hola nene, dame un numerin");
+        A = in.nextInt();
+
+        System.out.println("Muy bien, dame otro y te hago la potencia, este numero será la potencia del anterior");
+        B = in.nextInt(); 
+        for (int limit = 1; limit <= B; limit++){
+            resultado *= A;
+        }
+        System.out.println("La potencia de " + A + " que es " + B + " da como resultado " + resultado);
+    }
+}
+
+
