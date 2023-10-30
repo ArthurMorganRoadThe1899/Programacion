02a65d2341a62961c0eaa9de1a6a49cf1fb2d1c4
Joselito <tidusasdeloszanarkandabes@gmail.com>
2023-10-30 10:19:41 +0100

    Ejercicios del 7 al 10 acabados

---
+++ EjerciciosDeBuclesJava/B_JAA7.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class B_JAA7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int neg = 0, limit = 1, pos = 0;
+
+        for (limit = 1; limit < 101; limit++) {
+            System.out.println("Dame, dame, numerines \uD83C\uDFB5");
+
+            int numero = in.nextInt();
+
+            if (numero < 0) {
+                neg++;
+            }else if (numero > 0){
+                pos++;
+            }
+
+            System.out.println("Negativos actuales: " + neg);
+            System.out.println("Positivos actuales: " + pos);
+        }
+        System.out.println("");
+        System.out.println("RESULTADOS");
+        System.out.println("Negativos en total: " + neg);
+        System.out.println("Positivos en total: " + pos);
+
+    }
+}
+
+// Hecho por Jose Alba Arrufat //
---
+++ EjerciciosDeBuclesJava/B_JAA8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class B_JAA8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int neg = 0, limit = 1, pos = 0;
+
+        System.out.println("");
+        System.out.println("Hola, este es un programa que te cuenta números negativos y positivos hasta el infinito, si te cansas, al escribir 0 se acaba la ejecución");
+        for (limit = 1; limit != 0;) {
+            System.out.println("Dame, dame, numerines \uD83C\uDFB5");
+
+            int numero = in.nextInt();
+
+            if (numero < 0) {
+                neg++;
+            }else if (numero > 0){
+                pos++;
+            }else if (numero == 0){
+                break;
+            }
+
+            System.out.println("Negativos actuales: " + neg);
+            System.out.println("Positivos actuales: " + pos);
+        }
+        System.out.println("");
+        System.out.println("RESULTADOS");
+        System.out.println("Negativos en total: " + neg);
+        System.out.println("Positivos en total: " + pos);
+
+    }
+}
+
+// Hecho por Jose Alba Arrufat //
