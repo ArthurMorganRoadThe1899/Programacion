6e7a3abc27b583b01051912ca0d51c2e599f8b47
Josito <josalbarr@alu.edu.gva.es>
2023-11-03 16:41:21 +0100

    20 acabado, 21 en proceso

--- EjerciciosDeBuclesJavaII/B_JAA20.java
+++ EjerciciosDeBuclesJavaII/B_JAA20.java
@@ -3,21 +3,29 @@ import java.util.Scanner;
 
 public class B_JAA20 {
     public static void main(String[] args) {
-        int limit = 20, num;
+        int limit, multi, num;
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Dime un número, ire contando hasta el de manera chistosa, solo te dejo hasta 20");
-        num = in.nextInt();
-        boolean numMayZero = num < 0;
-        boolean numMenTwenty = num > 20;
-        boolean trueCondition = numMenTwenty || numMayZero;
+            try {
+                System.out.println("Dime un número, ire contando hasta el de manera chistosa, solo te dejo hasta 20");
+                num = in.nextInt();
+                boolean numMayZero = num < 0;
+                boolean numMenTwenty = num > 20;
+                boolean trueCondition = numMenTwenty || numMayZero;
 
-        if (!trueCondition)
-            for (num = 1; num <= limit; num++) {
-                System.out.println(num);
-        }else{
-            System.out.println("No");
-        }
+                if (!trueCondition) {
+                    for (limit = 1; limit <= num; limit++) {
+                        for (multi = 1; multi <= limit; multi++) {
+                            System.out.print(limit);
+                        }
+                        System.out.println("");
+                    }
+                } else {
+                    System.out.println("No");
+                }
 
+            } catch (Exception err) {
+                System.out.println("Pusiste algo mal loco");
+            }
     }
 }
---
+++ EjerciciosDeBuclesJavaII/B_JAA21.java
@@ -0,0 +1,26 @@
+package TareaJava.EjerciciosDeBuclesJavaII;
+import java.util.Scanner;
+
+public class B_JAA21 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num, num2;
+        System.out.println("Muy bien mi loco, esta vez te pediré que me digas un número, y yo te digo la diferencia que hay entre uno y otro");
+        System.out.println("");
+        System.out.println("Dame el primer número");
+        num = in.nextInt();
+        System.out.println("Dame otro, ha de ser mayor al anterior");
+        num2 = in.nextInt();
+
+        if(num2 < num){
+            System.out.println("");
+            System.out.println("Ñaca, te dije que fuese mayor que el anterior");
+        }else{
+            while (num2 > num){
+                num++;
+                System.out.print(num + " ");
+            }
+            int total = num + num2;
+        }
+    }
+}
