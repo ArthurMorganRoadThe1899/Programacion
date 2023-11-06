61a2079e81ec667b4ecf486317e37ccb88dd9918
Josito <josalbarr@alu.edu.gva.es>
2023-11-06 19:14:02 +0100

    D_JAA3 casi acabado, falta la parte de mostrar la el número más bajo, media y alto.

--- EjerciciosNumerosAleatorios/D_JAA3.java
+++ EjerciciosNumerosAleatorios/D_JAA3.java
@@ -1,7 +1,18 @@
 package Programacion.EjerciciosNumerosAleatorios;
-
+import java.util.Random;
 public class D_JAA3 {
     public static void main(String[] args) {
+        int contador;
+
+        System.out.println("Muy buenas, aquí tienes, 50 números random entre 100 y 199 mi rey");
+        System.out.println();
+
+        for (contador = 1; contador < 50; contador++){
+            Random rn = new Random();
+            int rNum = rn.nextInt(199) + 100;
+            System.out.print(rNum + " ");
+        }
+        System.out.println();
 
     }
 }
