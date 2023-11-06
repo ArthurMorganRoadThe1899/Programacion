2ebbbafd8701f639b0715ee415bd9214f5b07231
Josito <josalbarr@alu.edu.gva.es>
2023-11-06 19:24:11 +0100

    acabar D_JAA4

--- EjerciciosNumerosAleatorios/D_JAA4.java
+++ EjerciciosNumerosAleatorios/D_JAA4.java
@@ -1,7 +1,13 @@
 package Programacion.EjerciciosNumerosAleatorios;
+import java.util.Random;
 
 public class D_JAA4 {
     public static void main(String[] args) {
+        Random rn = new Random();
 
+            int rNum = rn.nextInt(100) + 0;
+
+        public static void randomizador(String[] args){
     }
 }
+
