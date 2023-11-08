473cbb01bdc4f23dd922722264402d0b648b5031
Joselito <tidusasdeloszanarkandabes@gmail.com>
2023-11-08 13:00:45 +0100

    Ejercicio 3 casi acabado, solo falta definir la media

--- EjerciciosNumerosAleatorios/D_JAA3.java
+++ EjerciciosNumerosAleatorios/D_JAA3.java
@@ -1,18 +1,44 @@
 package Programacion.EjerciciosNumerosAleatorios;
 import java.util.Random;
+import java.util.ArrayList;
 public class D_JAA3 {
     public static void main(String[] args) {
-        int contador;
+        int contador, rNum;
+        ArrayList<Integer> nums = new ArrayList<>();
+
 
         System.out.println("Muy buenas, aquí tienes, 50 números random entre 100 y 199 mi rey");
         System.out.println();
 
-        for (contador = 1; contador < 50; contador++){
+        for (contador = 0; contador < 50; contador++) {
             Random rn = new Random();
-            int rNum = rn.nextInt(199) + 100;
+            rNum = rn.nextInt(199) + 100;
             System.out.print(rNum + " ");
+            nums.add(rNum);
         }
         System.out.println();
 
+        int max = 0;
+        for (int checkDiscard = 0; checkDiscard < nums.size(); checkDiscard++) {
+            if (nums.get(checkDiscard) > max) {
+                max = nums.get(checkDiscard);
+            }
+        }
+        int min = max;
+        for (int checkDiscard = 0; checkDiscard < nums.size(); checkDiscard++) {
+            if (nums.get(checkDiscard) < min) {
+                min = nums.get(checkDiscard);
+            }
+        }
+        int mid = max;
+        for (int checkDiscard = 0; checkDiscard < nums.size(); checkDiscard++) {
+
+        }
+        System.out.println();
+        System.out.println("Mínimo: " + min);
+        System.out.println("Media: " + mid);
+        System.out.println("Máximo: " + max);
+
+
+        }
     }
-}
