c46480a62767f548f136e3ef46c9b3845bf3c57c
Josito <josalbarr@alu.edu.gva.es>
2023-11-09 19:14:49 +0100

    array java inicializado, ACABAR ANTES DEL LUNES

--- array.java
+++ array.java
@@ -1,7 +1,10 @@
 package Programacion;
 
+
 public class array {
     public static void main(String[] args) {
 
     }
 }
+
+// Hay 3 array, 2 de ellos guardan números (uno de ellos tiene menos números almacenados), y el tercer array se encargara de comparar los números más grandes entre ellos (es decir, en 0 el más grande sería este, en 1 este, y así)
