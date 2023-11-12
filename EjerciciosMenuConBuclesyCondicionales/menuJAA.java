a658d56315d725e7a520a523ea11f08c13229ca9
Joselito <tidusasdeloszanarkandabes@gmail.com>
2023-11-12 17:56:06 +0100

    Menu java iniciado

---
+++ EjerciciosMenuConBuclesyCondicionales/menuJAA.java
@@ -0,0 +1,18 @@
+package Programacion.EjerciciosMenuConBuclesyCondicionales;
+import java.util.Scanner;
+
+public class menuJAA {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("*******************************\n" +
+                "******* MENU DE OPCIONE'*******\n" +
+                "*******************************\n" +
+                "1. Permutar dos valores.\n" +
+                "2. Sumatorio.\n" +
+                "3. Máximo y mínimo.\n" +
+                "4. Primeros números.\n" +
+                "5. Irse.\n" +
+                "*******************************");
+
+    }
+}
