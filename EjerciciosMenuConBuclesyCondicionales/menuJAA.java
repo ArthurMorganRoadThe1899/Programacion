6f4774e77eaf1d5d018d40ac4ea5b6f55346c6cc
Joselito <tidusasdeloszanarkandabes@gmail.com>
2023-11-12 18:11:26 +0100

    Menu java en proceso

--- EjerciciosMenuConBuclesyCondicionales/menuJAA.java
+++ EjerciciosMenuConBuclesyCondicionales/menuJAA.java
@@ -14,5 +14,14 @@ public class menuJAA {
                 "5. Irse.\n" +
                 "*******************************");
 
+        System.out.println();
+        System.out.println("1. Te pediré dos números, y te los mostraré en pantalla de abajo a arriba en orden en el que me los diste");
+        System.out.println("2. Dame dos valores y te los sumo loco");
+        System.out.println("3. Dame cuantos valores quieras, y te diré cuál es el más grande de todos");
+        System.out.println("4. Dame un número y te mostraré la sucesión de todos sus números previos a el");
+        System.out.println("5. Acabas la ejecución del programa");
+
+
+
     }
 }
