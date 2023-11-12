19c8d0ce4d005e61853749909468f6f68e2b98e7
Joselito <tidusasdeloszanarkandabes@gmail.com>
2023-11-12 12:04:55 +0100

    Array.java en proceso

--- array.java
+++ array.java
@@ -1,16 +1,31 @@
 package Programacion;
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class array {
     public static void main(String[] args) {
-        int array1[] = {4, 5, 7, 1, 6, 7};
-        int array2[] = {3, 6, 6, 2, 4, 9, 9};
-        int array3[];
 
-        for (int contador = 0; contador < array2.length; contador++){
-            array3[] = Arrays.stream(array2).toArray();
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(4,5,7,1,6,7));
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3,6,6,2,4,9,9));
+        ArrayList<Integer> array3 = new ArrayList<>(Arrays.asList());
+
+        for (int checkDiscard = 0; checkDiscard < array2.size(); checkDiscard++) {
+            if (array2.get(checkDiscard) > array3.get(checkDiscard)) {
+                array3.get(checkDiscard) = array2.get(checkDiscard);
+
         }
+
+            if (array1.get(checkDiscard) > array3.get(checkDiscard)) {
+                array3.get(checkDiscard) = array1.get(checkDiscard);
+
+            }
+            System.out.println(array3);
+        }
+
     }
 }
+
+// https://stackoverflow.com/questions/4763974/directly-setting-values-for-an-arraylist-in-java
 
 // Hay 3 array, 2 de ellos guardan números (uno de ellos tiene menos números almacenados), y el tercer array se encargara de comparar los números más grandes entre ellos (es decir, en 0 el más grande sería este, en 1 este, y así)
