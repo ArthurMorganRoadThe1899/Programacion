2d349d90b2a2488b9a73c9a9f8a72838b0a4aad3
Josito <josalbarr@alu.edu.gva.es>
2023-11-22 20:28:45 +0100

    tarea array iniciada, en punto b

--- Java/cArray.java
+++ Java/cArray.java
@@ -4,16 +4,30 @@ public class cArray {
     public static void main(String[] args) {
         int[] arr = new int[10];
         int total = 0;
+        int mid = 0;
 
             for (int i = 0; i < arr.length; i++){
                 arr[i] = (int) (Math.random()*30);
             }
-        System.out.print(arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5]);
+
+            for (int i = 0; i < arr.length; i++){
+                System.out.print(arr[i] + " ");
+            }
+        // System.out.print(arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5]);
         
         for (int i = 0; i < arr.length; i++){
             total += arr[i];
         }
-        System.out.println(" " + total);
+
+        for (int i = 0; i < arr.length; i++) {
+            mid = mid + arr[i];
+        }
+
+        mid = mid / arr.length;
+
+        System.out.println();
+        System.out.println("Sumatorio: " + total);
+        System.out.println("Mediana: " + mid);
         
     }
 }
