510bd5717c2bd092882cd1c1278353c7da09b2fd
Josito <josalbarr@alu.edu.gva.es>
2023-11-28 17:44:04 +0100

    cesar

---
+++ Java/cesar/cifradoCesar.java
@@ -0,0 +1,12 @@
+package Programacion.Java.cesar;
+import java.util.Scanner;
+
+public class cifradoCesar {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char xd = 'a';
+
+        System.out.println("¿Que tipo de cifrado César quieres utilizar?");
+        int rC = rC;
+    }
+}
