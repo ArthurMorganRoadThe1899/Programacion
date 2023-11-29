80b5dfedbf752d0e8e565e8413adbf6f87f6003c
Joselito <tidusasdeloszanarkandabes@gmail.com>
2023-11-29 21:25:42 +0100

    Ejercicio de cifrado César en proceso

--- Java/cesar/cifradoCesar.java
+++ Java/cesar/cifradoCesar.java
@@ -2,7 +2,7 @@ package Programacion.Java.cesar;
 import java.util.Scanner;
 
 public class cifradoCesar {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         char[] abecedario = new char[] {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         String rS;
