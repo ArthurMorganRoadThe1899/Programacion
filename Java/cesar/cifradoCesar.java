33a81e814cb9aca97be215ef7c48b7bc9f54ddb9
Joselito <tidusasdeloszanarkandabes@gmail.com>
2023-11-30 16:42:52 +0100

    Ejercicio de cifrado César en proceso

--- Java/cesar/cifradoCesar.java
+++ Java/cesar/cifradoCesar.java
@@ -6,6 +6,7 @@ public class cifradoCesar {
         Scanner in = new Scanner(System.in);
         char[] abecedario = new char[] {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
         String rS;
+        int rC = 0;
 
         System.out.println("Dime una palabra, y la cifrare con el cifrado César de la magnitud de desplazamiento que busques.");
 
@@ -16,7 +17,13 @@ public class cifradoCesar {
         //}while(); // Arreglar este while, he de hacer que pida obligatoriamentem que rS tenga solo letters
 
         System.out.println("¿Que tipo de cifrado César quieres utilizar?");
-        int rC = in.nextInt(); // Número de rotación
+        do {
+            try {
+                rC = in.nextInt(); // Número de rotación
+            }catch (Exception e){
+                System.out.println("Solo acepto números enteros");
+            }
+        }while(rC >= 26); // Este do while es para que no pasede 25, ya que es el tope, si se pone 26 hace mirror y vuelve al valor original
 
         char [] rCS = rS.toCharArray(); // array de carácteres basado en la palabra a cifrar
 
@@ -30,6 +37,9 @@ public class cifradoCesar {
             /*if(abecedario[b].equals(" ")){
                 rCS[i] = abecedario[0];
             }else{
+
+            }*/
+            /*if(rCS = 32 ){
 
             }*/
             rCS[i] = abecedario[b + rC];
