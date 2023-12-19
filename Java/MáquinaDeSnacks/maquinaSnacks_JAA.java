00073ac1a8d776d70f3208b59bf80e155d6f8e9f
joselito <tidusasdeloszanarkandabes@gmail.com>
2023-12-19 23:12:57 +0100

    Máquina de snacks en proceso, parte visual inicial preparada

--- Java/MáquinaDeSnacks/maquinaSnacks_JAA.java
+++ Java/MáquinaDeSnacks/maquinaSnacks_JAA.java
@@ -1,6 +1,35 @@
 package Programacion.Java.MáquinaDeSnacks;
 import java.util.Scanner;
 public class maquinaSnacks_JAA {
-    int maquina[][] = new int [4][4];
+    public static void main(String[] args) {
+        int maquina[][] = new int [3][3];
+        Scanner in = new Scanner(System.in);
+        String spa;
+        System.out.println("\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⠀⢀⣀⡀⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣿⡟⠛⣿⣿⠋⣿⣿⣿⣿⣿⠀⣿⠉⣿⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣏⣁⣀⣉⣁⣀⣉⣉⣉⣉⣿⠀⠿⠶⠿⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣿⣿⣿⣿⣿⣿⡿⠛⢿⣿⣿⠀⠰⠶⠆⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣯⣭⣭⣭⣭⣭⣥⣤⣬⣭⣿⠀⠐⠶⠆⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣿⡟⢿⣿⠁⣿⣿⠉⢹⣿⣿⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣧⣤⣤⣤⣤⣤⣤⣤⣤⣤⣿⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣿⠉⠙⣿⠉⠉⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠰⠶⠆⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣉⣉⣉⣉⣉⣉⣉⣉⣉⣉⣉⣉⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⣿⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⢻⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⠀⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
+                "\n Vaya, te has topado con una máquina expendedora salvaje\n" +
+                "\n[DALE A ENTER]");
 
+            spa = in.nextLine();
+
+        System.out.println("\n¿Qué snack te hace pillarte?\n" +
+                "FILA 0: 0-Chocapic | 1-Sugus | 2-Schokobons\n" +
+                "FILA 1: 0-Papas | 1-Pipas | 2-Nueces\n" +
+                "FILA 2: 0-Coca-cola | 1-Monster | 2-Agua\n");
+
+
+    }
 }
