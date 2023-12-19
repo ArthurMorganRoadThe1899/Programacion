b2f01dc80a1113517a1f0e67c297adb29ac6b6f7
joselito <tidusasdeloszanarkandabes@gmail.com>
2023-12-19 22:50:19 +0100

    Máquina de snacks iniciada, archivo java de flotaHundida iniciado

---
+++ Java/HundirLaFlota/flotaHundida.java
@@ -0,0 +1,5 @@
+package Programacion.Java.HundirLaFlota;
+import java.util.Scanner;
+public class flotaHundida {
+    //int t[][] = new int [][];
+}
---
+++ Java/MáquinaDeSnacks/maquinaSnacks_JAA.java
@@ -0,0 +1,6 @@
+package Programacion.Java.MáquinaDeSnacks;
+import java.util.Scanner;
+public class maquinaSnacks_JAA {
+    int maquina[][] = new int [4][4];
+
+}
