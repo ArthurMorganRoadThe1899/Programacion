4305627f679e81fb92c67177859371d5e185effb
Joselito <tidusasdeloszanarkandabes@gmail.com>
2023-12-20 17:14:25 +0100

    hundir la flota en proceso

--- Java/HundirLaFlota/README.md
+++ Java/HundirLaFlota/README.md
@@ -1,7 +1,26 @@
-  Defensa del projecte final
+## DATOS IMPORTANTES
 
-    * A partir de gener, l'alumnat defensarà el seu projecte.
-    * L'ordre serà aleatori.
-    * Es recorda que totes les funcions han de tindre una descripció de la seua funcionalitat abans de la capçalera.
-    * El codi ha d'estar comentat.
-    * L'alumnat ha de demostrar que domina les estructures definides en el seu propi codi, així com tota la funcionalitat.
+    * Añadir barcos
+        - Lancha (1 casilla)
+        - Barco  (3 casillas --)
+        - Acorazado (4 casillas --)
+        - Portaaviones (5 casillas |)
+        
+    * Nivel de dificultad
+        - Fácil
+            Se colocarán 10 barcos aleatoriamente (5 lanchas | 3 barcos | 1 acorazado | 1 portaaviones)
+                * 50 intentos
+            
+        - Media
+            Se colocarán 5 barcos aleatoriamente (2 lanchas | 1 barcos | 1 acorazado | 1 portaaviones)
+                * 30 intentos
+            
+        - Difícil
+            Se colocarán 2 barcos aleatoriamente (1 lanchas | 1 barcos)
+                * 10 intentos
+            
+        - Personalizado
+            * Longitud del tablero personalizado a gusto de jugador
+            * Cantidad de barcos
+            * Cantidad de intentos
+        
--- Java/HundirLaFlota/flotaHundida.java
+++ Java/HundirLaFlota/flotaHundida.java
@@ -1,5 +1,42 @@
 package Programacion.Java.HundirLaFlota;
 import java.util.Scanner;
 public class flotaHundida {
-    //int t[][] = new int [][];
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[][] tablero = new int[10][10];
+        int[][] tableroO = new int[10][10];
+        String enter;
+        int difficulty;
+
+
+        System.out.println("\n" +
+                "\n" +
+                "888             888   888   888                888     d8b         \n" +
+                "888             888   888   888                888     Y8P         \n" +
+                "888             888   888   888                888                 \n" +
+                "88888b.  8888b. 888888888888888 .d88b. .d8888b 88888b. 88888888b.  \n" +
+                "888 \"88b    \"88b888   888   888d8P  Y8b88K     888 \"88b888888 \"88b \n" +
+                "888  888.d888888888   888   88888888888\"Y8888b.888  888888888  888 \n" +
+                "888 d88P888  888Y88b. Y88b. 888Y8b.         X88888  888888888 d88P \n" +
+                "88888P\" \"Y888888 \"Y888 \"Y888888 \"Y8888  88888P'888  88888888888P\"  \n" +
+                "                                                          888      \n" +
+                "                                                          888      \n" +
+                "                                                          888      \n" +
+                "\n");
+
+        System.out.println("[PRESIONA CUALQUIER BOTÓN PARA EMPEZAR]");
+            enter = in.nextLine();
+
+        System.out.println("#########################\n" +
+                "###-MENU-DE-DIFICULTAD-##\n" +
+                "@1·-FÁCIL---------------#\n" +
+                "@2·-NORMAL--------------#\n" +
+                "@3·-DIFÍCIL-------------#\n" +
+                "@4·-PERSONALIZADO-------#\n" +
+                "#########################\n");
+        System.out.println("SELECCIONA ALGUNA DE ESTAS DIFICULTADES \n[1 | 2 | 3 | 4]");
+        //do {
+            difficulty = in.nextInt();
+        //}while()
+    }
 }
