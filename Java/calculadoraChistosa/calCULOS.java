75fc5575b05a46a5489e0f1ccd8772cab7341875
joselito <tidusasdeloszanarkandabes@gmail.com>
2024-02-10 00:40:39 +0100

    Matematica, aiuda

--- Java/calculadoraChistosa/calCULOS.java
+++ Java/calculadoraChistosa/calCULOS.java
@@ -6,12 +6,41 @@ public class calCULOS {
     static final int c = 6;
     static final int d = 7;
 
-    public int suma(){
+    public static int suma(){
         int total1 = a + c;
         int total2 = d + b;
         System.out.println(a + " + " + c + " = " + total1 +
-                "\n " + d + " + " + b + " = " + total2);
-        return(total1, total2);
+                "\n" + d + " + " + b + " = " + total2 +
+                "\ntotal: " + (total1 +  total2));
+        return(total1);
+    }
+    public static int resta(){
+        int total1 = a - c;
+        int total2 = d - b;
+        System.out.println(a + " - " + c + " = " + total1 +
+                "\n" + d + " - " + b + " = " + total2 +
+                "\ntotal: " + (total1 +  total2));
+        return(total1);
+    }
+
+    public static int multipli(){
+        int total1 = a + c;
+        int total2 = d + b;
+        int total3 = a + d;
+        int total4 = c + b;
+        System.out.println(a + " + " + c + " = " + total1 +
+                "\n" + d + " + " + b + " = " + total2 +
+                "\ntotal: " + (total1 +  total2));
+        return(total1);
+    }
+
+    public static int division(){
+        int total1 = a + c;
+        int total2 = d + b;
+        System.out.println(a + " + " + c + " = " + total1 +
+                "\n" + d + " + " + b + " = " + total2 +
+                "\ntotal: " + (total1 +  total2));
+        return(total1);
     }
 
 
--- Java/calculadoraChistosa/calculadora.java
+++ Java/calculadoraChistosa/calculadora.java
@@ -1,8 +1,42 @@
 package Programacion.Java.calculadoraChistosa;
-
+import java.util.Scanner;
 public class calculadora {
+    static final Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
+        int res = 0;
+        boolean z;
+            System.out.println("Bienvenido a la caluladora sucio pobre, elige que quieres hacer:\n" +
+                    "======================\n" +
+                    "====== 1. Sumar ======\n" +
+                    "===== 2. Restar ======\n" +
+                    "=== 3 . Multiplicar ==\n" +
+                    "===== 4. Dividir =====\n" +
+                    "======================\n" +
+                    "\nElige entre estas cuatro opciones\n");
 
-        sout
+            do {
+                try {
+                    res = Integer.parseInt(in.nextLine());
+                    z = false;
+                } catch (Exception err) {
+                    z = true;
+                }
+                if(res != 1 && res != 2 && res != 3 && res != 4 || z){
+                    System.out.println("Valor introducido no valido");
+                    z = true;
+                }
+            }while(z);
+
+            if (res == 1) {
+                calCULOS.suma();
+            } else if (res == 2) {
+                calCULOS.resta();
+            } else if (res == 3) {
+                calCULOS.multipli();
+            } else if(res == 4) {
+                calCULOS.division();
+            }
+
+
     }
 }
