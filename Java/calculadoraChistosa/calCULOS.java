36d7ee439cc479c67d4fe2bf67ee1331a77b345b
joselito <tidusasdeloszanarkandabes@gmail.com>
2024-02-10 18:21:58 +0100

    Matematica finalizada

--- Java/calculadoraChistosa/calCULOS.java
+++ Java/calculadoraChistosa/calCULOS.java
@@ -1,46 +1,47 @@
 package Programacion.Java.calculadoraChistosa;
 
 public class calCULOS {
-    static final int a = 5;
-    static final int b = 2;
-    static final int c = 6;
-    static final int d = 7;
+    static int a = 5;
+    static int b = 2;
+    static int c = 6;
+    static int d = 7;
+    static int n = 2;
 
-    public static int suma(){
+    public calCULOS(int a, int b, int c, int d, int n){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.n = n;
+
+    }
+
+    public static void suma(){
         int total1 = a + c;
         int total2 = d + b;
         System.out.println(a + " + " + c + " = " + total1 +
                 "\n" + d + " + " + b + " = " + total2 +
-                "\ntotal: " + (total1 +  total2));
-        return(total1);
+                "\n(" + a + " + " + c + ") + (" + d + " + " + b + ") = " + (total1 + total2));
     }
-    public static int resta(){
+    public static void resta(){
         int total1 = a - c;
         int total2 = d - b;
         System.out.println(a + " - " + c + " = " + total1 +
                 "\n" + d + " - " + b + " = " + total2 +
-                "\ntotal: " + (total1 +  total2));
-        return(total1);
+                "\n(" + a + " - " + c + ") (" + d + " - " + b + ") = " + (total1 - total2));
     }
 
-    public static int multipli(){
-        int total1 = a + c;
-        int total2 = d + b;
-        int total3 = a + d;
-        int total4 = c + b;
-        System.out.println(a + " + " + c + " = " + total1 +
-                "\n" + d + " + " + b + " = " + total2 +
-                "\ntotal: " + (total1 +  total2));
-        return(total1);
+    public static void multipli1(){
+        int total1 = (a * c - b * d) * (a * d + b * c);
+        System.out.println("(" + a + " * " + c + " - " + b + " * " + d + ") * (" + a + " * " + d + " + " + b  + " * " + c + ") = " + total1);
     }
 
-    public static int division(){
-        int total1 = a + c;
-        int total2 = d + b;
-        System.out.println(a + " + " + c + " = " + total1 +
-                "\n" + d + " + " + b + " = " + total2 +
-                "\ntotal: " + (total1 +  total2));
-        return(total1);
+    public static void multipli2(){
+        System.out.println("("+  a + ", " + b + ") * " + n + " = " + "("+ a*n + ", " + b*n + ") ");
+    }
+
+    public static void laTeoriaDeLadivision(){
+        System.out.println("(a, b) / (c, d) = ((" + a + " * " + c + " + " + b + " * " + d + ") / (" + Math.pow(c,2) + " + " + Math.pow(d,2) + ", (" + b + " * " + c + " - " + a + " * " + d + ") / (" + Math.pow(c,2) + " + " + Math.pow(d,2) + "))");
     }
 
 
--- Java/calculadoraChistosa/calculadora.java
+++ Java/calculadoraChistosa/calculadora.java
@@ -4,8 +4,9 @@ public class calculadora {
     static final Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         int res = 0;
+        int res2 = 0;
         boolean z;
-            System.out.println("Bienvenido a la caluladora sucio pobre, elige que quieres hacer:\n" +
+            System.out.println("Bienvenido a la caluladora, elige que quieres hacer:\n" +
                     "======================\n" +
                     "====== 1. Sumar ======\n" +
                     "===== 2. Restar ======\n" +
@@ -27,16 +28,52 @@ public class calculadora {
                 }
             }while(z);
 
+            /*SUMA*/
             if (res == 1) {
+                calCULOS datos = new calCULOS(1, 2, 3, 4, 5);
                 calCULOS.suma();
+
+            /*RESTA*/
             } else if (res == 2) {
+                calCULOS datos = new calCULOS(1, 2, 3, 4, 5);
                 calCULOS.resta();
+
+            /*MULTIPLICAR*/
             } else if (res == 3) {
-                calCULOS.multipli();
+                calCULOS datos = new calCULOS(1, 2, 3, 4, 5);
+                System.out.println("\nMultiplicación compleja (1) o multiplicar muchos números por " + calCULOS.n + " (2)");
+                do {
+                    try {
+                        res2 = Integer.parseInt(in.nextLine());
+                        z = false;
+                    } catch (Exception err) {
+                        z = true;
+                    }
+
+                    if(res2 != 1 && res2 != 2 || z){
+                        System.out.println("Valor introducido no valido");
+                        z = true;
+                    }
+
+                }while(z);
+
+                if(res2 == 1){
+                    calCULOS.multipli1();
+                }else if(res2 == 2){
+                    calCULOS.multipli2();
+                }
+
+
+            /*DIVIDIR*/
             } else if(res == 4) {
-                calCULOS.division();
+                calCULOS datos = new calCULOS(456, 2, 3, 4, 5);
+                calCULOS.laTeoriaDeLadivision();
             }
 
+        int pachucho = (int) (Math.random() * 100) + 1;
 
+            if(pachucho == 47){
+                System.out.println("https://sprites.pmdcollab.org/");
+            }
     }
 }
