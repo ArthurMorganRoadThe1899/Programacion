1d1015e6cf701a32b84ac5ea23d4e2e0894cbf41
JohnMarston <josalbarr@alu.edu.gva.es>
2024-02-20 20:33:05 +0100

    Agenda en progreso

--- Java/Agenda/agendaThings.java
+++ Java/Agenda/agendaThings.java
@@ -1,4 +1,18 @@
 package Programacion.Java.Agenda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class agendaThings {
+    public static void persona(){
+        ArrayList<String> DNI = new ArrayList<>(Arrays.asList("123456789A", "123456789B", "123456789C", "123456789D"));
+        ArrayList<String> name = new ArrayList<>(Arrays.asList("Vira", "Alberto", "Martin", "Gale"));
+        ArrayList<String> surname = new ArrayList<>(Arrays.asList("Von Goach", "el Hambriento", "Roque Alba", "de Waterdeep"));
+        ArrayList<String> pnum = new ArrayList<>(Arrays.asList("258 256 70 78", "134 763 23 86", "235 852 45 95", "178 435 34 12"));
+        ArrayList<String> email = new ArrayList<>(Arrays.asList("viragoach@gmail.com", "alberto@galactus.com", "martinroquealba@consolacionnules.es", "galewateerdeep@hotmail.com"));
+
+    }
+
+
+
 }
