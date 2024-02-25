a084c0b8f172c446a1b98199460beeac10ba827f
joselito <tidusasdeloszanarkandabes@gmail.com>
2024-02-25 17:07:20 +0100

    Cosa finalizada

--- Java/animalicos/AppAnimales.java
+++ Java/animalicos/AppAnimales.java
@@ -11,33 +11,33 @@ public class AppAnimales {
     /*static  Caballo c1 = new Caballo(Caballo.edad, Caballo.nombre, Caballo.tipo_alimentacion, Caballo.patas);
     static  Gato g1 = new Gato(Gato.edad, Gato.nombre, Gato.tipo_alimentacion, Gato.pedigri);
     static Perro p1 = new Perro(Perro.edad, Perro.nombre, Perro.tipo_alimentacion, Perro.raza);*/
-    static int r, r2;
+    static int respuesta, respuesta2;
     public static void main(String[] args){
         System.out.println("\nHola amable usuario ¿qué le gustaría hacer? :3\n\n");
         do{
         System.out.println("=========================================================\n" +
                 "==== 1. VER INFORMACIÓN DE LOS ANIMALES EN LA GRANJA ====\n" +
                 "==== 2. MODIFICAR ANIMALICOS ============================\n" +
-                "==== 3. COMPARADOR EDADES ENTRE ANIMALES ================\n"+
+                "==== 3. COMPARADOR DE EDADES ENTRE ANIMALES =============\n"+
                 "==== 4. SALIR ===========================================\n" +
                 "=========================================================\n");
 
         boolean tf = false;
         do {
             try {
-                r = parseInt(in.nextLine());
+                respuesta = parseInt(in.nextLine());
                 tf = false;
             }catch(Exception err){
                 tf = true;
             }
-            if(r <1 || r > 4){
+            if(respuesta <1 || respuesta > 4){
                 System.out.println("Valor introducido no valido");
                 tf = true;
             }
         }while(tf);
 
         /*1. MOSTRAR A TODOS LOS BICHOS Y SUS RASGOS*/
-        if(r == 1){
+        if(respuesta == 1){
             new Caballo(Caballo.edad, Caballo.nombre, Caballo.tipo_alimentacion, Caballo.patas);
             System.out.print("CABALLO:");
             Caballo.info();
@@ -51,12 +51,12 @@ public class AppAnimales {
             Perro p1 = new Perro(Perro.edad, Perro.nombre, Perro.tipo_alimentacion, Perro.raza);
             System.out.print("PERRO:");
             Perro.info();
-            System.out.println("Patas: " + Perro.raza +"\n");
+            System.out.println("Raza: " + Perro.raza +"\n");
 
         /*2. MODIFICAR ANIMALICOS*/
-        }else if(r == 2){
-            System.out.println("Muy bien, a quien quieres modificar?\n" +
-                    "====================\n" +
+        }else if(respuesta == 2){
+            System.out.println("Muy bien, a quien quieres modificar?" +
+                    "\n====================\n" +
                     "==== 1. CABALLO ====\n" +
                     "==== 2. GATO =======\n" +
                     "==== 3. PERRO ======\n" +
@@ -64,65 +64,66 @@ public class AppAnimales {
             boolean tf2 = false;
             do {
                 try {
-                    r2 = parseInt(in.nextLine());
+                    respuesta2 = parseInt(in.nextLine());
                     tf2 = false;
                 }catch(Exception err){
                     tf2 = true;
                 }
-                if(r != 1 && r != 2 && r != 3){
+                if(respuesta < 1 || respuesta > 3){
                     System.out.println("Valor introducido no valido");
                     tf2 = true;
                 }
             }while(tf);
 
-            if(r2 == 1){
+            if(respuesta2 == 1){
                 System.out.println("Muy bien, adelante, modifica al caballo\nNOMBRE:");
                 Caballo.nombre = in.nextLine();
-                System.out.println("EDAD:");
-                Caballo.edad = in.nextInt();
                 System.out.println("TIPO DE ALIMENTACIÓN:");
                 Caballo.tipo_alimentacion = in.nextLine();
                 System.out.println("PATAS:");
                 Caballo.patas = in.nextInt();
-            }else if(r2 == 2){
+                System.out.println("EDAD:");
+                Caballo.edad = in.nextInt();
+            }else if(respuesta2 == 2){
+                System.out.println("Muy bien, adelante, modifica al gatico\nNOMBRE:");
+                Gato.nombre = in.nextLine();
+                System.out.println("TIPO DE ALIMENTACIÓN:");
+                Gato.tipo_alimentacion = in.nextLine();
+                System.out.println("PEDIGRI:");
+                Gato.pedigri = in.nextLine();
+                System.out.println("EDAD:");
+                Gato.edad = in.nextInt();
+            }else if(respuesta2 == 3){
                 System.out.println("Muy bien, adelante, modifica al perrico\nNOMBRE:");
                 Perro.nombre = in.nextLine();
-                System.out.println("EDAD:");
-                Perro.edad = in.nextInt();
                 System.out.println("TIPO DE ALIMENTACIÓN:");
                 Perro.tipo_alimentacion = in.nextLine();
                 System.out.println("RAZA:");
                 Perro.raza = in.nextLine();
-            }else if(r2 == 3){
-                System.out.println("Muy bien, adelante, modifica al gatico\nNOMBRE:");
-                Gato.nombre = in.nextLine();
                 System.out.println("EDAD:");
-                Gato.edad = in.nextInt();
-                System.out.println("TIPO DE ALIMENTACIÓN:");
-                Gato.tipo_alimentacion = in.nextLine();
-                System.out.println("Pedigri:");
-                Gato.pedigri = in.nextLine();
+                Perro.edad = in.nextInt();
             }
         /*3. VER LAS EDADES ENTRE LOS ANIMALICOS*/
-        }else if(r == 3){
+        }else if(respuesta == 3){
             System.out.println("Las edades son las siguientes:\n" +
                     "Caballo: " + Caballo.edad + " | Gato: " + Gato.edad + " | Perro: " + Perro.edad + "\n");
             ArrayList<Integer> papeadaDeMameo = new ArrayList<Integer>(Arrays.asList(Caballo.edad, Gato.edad, Perro.edad));
 
             int mayor = 0;
-            int menor = 0;
-            for(int i = 9; i < papeadaDeMameo.size(); i++){
+            int menor = 999999999;
+            for(int i = 0; i < papeadaDeMameo.size(); i++){
                 if(papeadaDeMameo.get(i) > mayor){
-                    mayor == papeadaDeMameo.get(i);
+                    mayor = papeadaDeMameo.get(i);
                 }
             }
-
+            for(int i = 0; i < papeadaDeMameo.size(); i++){
+                if(papeadaDeMameo.get(i) < menor){
+                    menor = papeadaDeMameo.get(i);
+                }
+            }
+            System.out.println("La mayor edad es " + mayor + ", y la menor es " + menor);
         }
 
-
-
-
-        }while(r != 4);
-
+        }while(respuesta != 4);
     }
 }
