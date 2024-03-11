8c6f62a8984b4332accb52f5d84d362a784de51f
Joselito <josalbarr@alu.edu.gva.es>
2024-03-11 17:49:15 +0100

    Vidas añadidas al advinador mágico, mu cutre to

--- Java/AdvinadorMagico/Joc.java
+++ Java/AdvinadorMagico/Joc.java
@@ -1,8 +1,18 @@
 package Programacion.Java.AdvinadorMagico;
 
 public class Joc {
+    static int vidas;
+
+    public static String mostrarVidasRestantes(){
+        return "Vidas: "+vidas;
+    }
+
     public static void main(String[] args) {
+        vidas = 5;
+        System.out.println(mostrarVidasRestantes());
+
 
     }
+
 
 }
