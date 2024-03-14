648900a3ed721aad04335ae8ae696ed9f148de72
joselitor <tidusasdeloszanarkandabes@gmail.com>
2024-03-14 16:35:40 +0100

    Juegico in progress

--- Java/AdvinadorMagico/IJugable.java
+++ Java/AdvinadorMagico/IJugable.java
@@ -3,7 +3,7 @@ package Programacion.Java.AdvinadorMagico;
 public interface IJugable {
     public void jugar();
 
-    public String mostrarNombre(String nombre);
+    public String mostrarNombre();
 
     public String descripcion();
 
--- Java/AdvinadorMagico/JocEndevinaNumero.java
+++ Java/AdvinadorMagico/JocEndevinaNumero.java
@@ -1,4 +1,5 @@
 package Programacion.Java.AdvinadorMagico;
+import java.util.Scanner;
 
 public class JocEndevinaNumero extends Joc implements IJugable {
 
@@ -11,21 +12,34 @@ public class JocEndevinaNumero extends Joc implements IJugable {
 
     @Override
     public void jugar() {
+        Scanner in = new Scanner(System.in);
         vidas = 5;
-        for(int i = 0; i < 5; i++){
-            mostrarVidasRestantes();
+        int numeroMagico = 2;
+
+        System.out.println(mostrarNombre() + descripcion() + "Muy bien hijo, ¿qué número crees que he maquinao'?");
+
+        int respuestaJugador = in.nextInt();
+
+        if(respuestaJugador == numeroMagico){
+            System.out.println("Ganaste");
+        }else if(respuestaJugador != numeroMagico){
+            quitarVida();
+            // SOUT DE ABAJO DE PRUEBA
+            System.out.println(vidas);
+        }else if(vidas == 0){
+            reiniciarPartida();
         }
-        vidas = vidas - 1;
     }
 
     @Override
-    public String mostrarNombre(String nombre) {
-        nombre = "EL VERDADERO ADIVINADOR";
-        return "BIENVENIDO A " + nombre;
+    public String mostrarNombre() {
+        return  "\n/=============================================/\n" +
+                "/==== BIENVENIDO A EL VERDADERO ADIVINADOR ===/\n" +
+                "/=============================================/";
     }
 
     @Override
     public String descripcion(){
-        return descripcion();
+        return "\n[ES UN JUEGO DE ADIVINACIÓN, TIENES QUE ADIVINAR UN NÚMERO DEL 0 AL 10]\n";
     }
 }
