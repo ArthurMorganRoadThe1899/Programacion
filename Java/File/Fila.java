5c56e0cb607bf98a86686add4dc476405a62bde1
Josito <josalbarr@alu.edu.gva.es>
2024-04-18 18:42:02 +0200

    wr

--- Java/File/Fila.java
+++ Java/File/Fila.java
@@ -1,4 +1,14 @@
 package Programacion.Java.File;
+import java.io.File;
 
 public class Fila {
+    public static void main(String[] args) {
+        File culo = new File("/home/josalbarr/Documentos/Tareas/Programacion/Java/File/a.txt");
+
+        if(culo.exists()){
+            System.out.println("tu madre");
+        }else{
+            System.out.println("no existe");
+        }
+    }
 }
