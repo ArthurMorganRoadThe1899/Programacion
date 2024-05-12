6d9f55235a33157063299f8ed9c488a16d71538c
Joselito <josalbarr@alu.edu.gva.es>
2024-05-12 21:04:14 +0200

    casi acabao

--- Java/FileTerminal/MiniFileManager.java
+++ Java/FileTerminal/MiniFileManager.java
@@ -1,6 +1,9 @@
 package Programacion.Java.FileTerminal;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,62 +15,109 @@ public class MiniFileManager {
         return in.nextLine();
     }
 
-    public static boolean changeDir(String dir){
 
-     return true;
+    // CD //
+    public static File changeDir(File ruta, String[] whereAreYouGoing){
+        File rutaBackUp = ruta;
+        // Aquí se que podría implementar un String[] más que tenga en cuenta los backslash para con un while o algo así, tirar pa' tras siempre, pero como ruta ya funciona decir ../../../ en la ruta final por ejemplo xD
+        if(whereAreYouGoing[1].equals("..") || whereAreYouGoing[1].equals("../") ){
+            ruta = ruta.getParentFile();
+        }else{
+            try{
+                ruta = new File(ruta + "/" + whereAreYouGoing[1]);
+                if(!ruta.exists()){
+                    System.out.println("la ruta que has puesto no existe");
+                    ruta = rutaBackUp;
+                }
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return ruta;
     }
 
 
     // CREAR DIRECTORIOS //
-    /*public static File createDirectory(File ruta, String[] directories){
-        File dir = new File(directoryPath);
-        if (dir.exists()) {
-            return dir;
-        }
-        if (dir.mkdirs()) {
-            return dir;
-        }
-        throw new IOException("Failed to create directory '" + dir.getAbsolutePath() + "' for an unknown reason.");
-    }*/
+    public static void createDirectory(File ruta, String[] directories) throws IOException {
+        Path directoryPath = Paths.get(ruta.toURI());
+        System.out.println(directoryPath);
+        File dir;
 
+        for(int i = 1; i < directories.length; i++) {
+            dir = new File(directoryPath + "/" + directories[i]);
 
-    // MÉTODO PARA QUE CUANDO LE PASAS PARÁMETROS AL COMANDO LS, TE DEVUELVA LOS ARCHIVOS QUE TE PASE Y TE LISTE EL CONTENIDO DE LOS ARCHIVOS QUE LE PASE //
-    // ! HAY QUE ARREGLAR ESTE CÓDIGO PORQUE FALLA LA IMPRESIÓN ! //
-    public static void lOut(String[] spaceDatainfo, File ruta, String tipoLs) throws FileNotFoundException {
-        File[] lista = new File[spaceDatainfo.length-1];
-
-        for(int i = 1; i < spaceDatainfo.length; i++){
-            File f = new File(ruta + spaceDatainfo[i]);
-            if(f.exists()){
-                lista = f.listFiles();
+            if (!dir.exists()) {
+                if (dir.mkdir()) {
+                    System.out.println("El directorio " + directories[i] + " ha sido creado");
+                } else if (!dir.exists()) {
+                    if (dir.mkdirs()) {
+                        System.out.println("Los directorios " + directories[i] + " han sido creados");
+                    } else {
+                        System.out.println("Algo fallo en la creación del o los directorios");
+                    }
+                }
             }
-        }
-
-
-
-        for (int i = 0; i < lista.length; i++) {
-            boolean infoExtra = !Objects.equals(tipoLs, "ls");
-
-            // MOSTRAR DIRECTORIOS Y ARCHIVOS //
-            if (lista[i].isDirectory()) {
-                printList(lista[i], infoExtra);
-            }
-
-            if (lista[i].isFile())
-                if (infoExtra)
-                    System.out.println("[A] " + lista[i].getName() + "\t" + lista[i].length() + " bytes\t" + new Date(lista[i].lastModified()));
-                else
-                    System.out.println("[A] " + lista[i].getName());
-
         }
     }
 
 
+    // MV [RENAME] //
+    public static boolean move(File ruta, String[] whatYouMoving){
+        boolean sanMovioOcambiaoDeName = true;
+
+            try {
+                Path source = Path.of(ruta + "/" + whatYouMoving[whatYouMoving.length-1]);
+                for(int i = 1; i <= whatYouMoving.length-1; i++){
+                    Path getWhatYouWantToMove = Path.of(ruta + "/" + whatYouMoving[i]);
+                    try {
+                        Files.move(getWhatYouWantToMove, source );
+                    }catch (Exception e){
+                        sanMovioOcambiaoDeName = false;
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                sanMovioOcambiaoDeName = false;
+                e.printStackTrace();
+            }
+
+        return sanMovioOcambiaoDeName;
+    }
+
+
+    // MÉTODO PARA QUE CUANDO LE PASAS PARÁMETROS AL COMANDO LS, TE DEVUELVA LOS ARCHIVOS QUE TE PASE Y TE LISTE EL CONTENIDO DE LOS ARCHIVOS QUE LE PASE //
+    public static void lOut(String[] spaceDatainfo, File ruta, String tipoLs) throws FileNotFoundException {
+        //File[] lista = new File[spaceDatainfo.length-1];
+        ArrayList<File> lista = new ArrayList<>();
+
+        for(int i = 1; i < spaceDatainfo.length; i++){
+            File f = new File(ruta + "/" + spaceDatainfo[i]);
+            if(f.exists()){
+                lista.add(f);
+            }
+        }
+
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println("\n" + lista.get(i).toString() + ":");
+            boolean infoExtra = !Objects.equals(tipoLs, "ls");
+
+            // MOSTRAR DIRECTORIOS Y ARCHIVOS //
+            if (lista.get(i).isDirectory()) {
+                printList(lista.get(i), infoExtra);
+            }
+
+            if (lista.get(i).isFile())
+                if (infoExtra)
+                    System.out.println("[A] " + lista.get(i).getName() + "\t" + lista.get(i).length() + " bytes\t" + new Date(lista.get(i).lastModified()));
+                else
+                    System.out.println("[A] " + lista.get(i).getName());
+
+        }
+    }
+    // Se que no hacía falta hacer esto, pero quería hacerlo porque en la consola de linux puedes pasarle como parámetros otros directorios para que los vaya imprimiento de uno en uno xD //
+
+
     // MÉTODO PARA MOSTRAR RUTAS //
-    // Te comento acá Vero, este es tu método que pusiste en un ejemplo, pero me pase un ratito investigando cosas de este y como funciona, obvio es, pero no sabía de los métodos de la clase File xD //
-    // Agarre y modifique un poco el método, a parte de de cambiar nombres de variables y mensajes, modifique la parte de escupir archivos y ficheros //
-    // no me gustaba que primero mostrase directorios y después archivos, hice un batiburrillo de mostrar alfábeticamente directorios y ficheros... //
-    // básicamente como en linux cuando haces ls a secas xD //
     public static void printList(File f, boolean infoExtra) throws FileNotFoundException {
 
         // LANZAR ERROR EN CASO DE QUE NO EXISTA EL ARCHIVO //
@@ -102,6 +152,8 @@ public class MiniFileManager {
 
     }
 }
-
-// Será útil mirar las expresiones regulares:
-// https://www.w3schools.com/java/java_regex.asp
+// Te comento acá Vero, este es tu método que pusiste en un ejemplo, pero me pase un ratito investigando cosas de este y como funciona, obvio es, //
+// pero no sabía de los métodos de la clase File xD //
+// Agarre y modifique un poco el método, a parte de de cambiar nombres de variables y mensajes, modifique la parte de escupir archivos y ficheros //
+// no me gustaba que primero mostrase directorios y después archivos, hice un batiburrillo de mostrar alfábeticamente directorios y ficheros... //
+// básicamente como en linux cuando haces ls a secas xD //
--- Java/FileTerminal/MiniTerminal.java
+++ Java/FileTerminal/MiniTerminal.java
@@ -2,14 +2,16 @@ package Programacion.Java.FileTerminal;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 
-public class    MiniTerminal {
-    public static void main(String[] args) throws FileNotFoundException {
+public class MiniTerminal {
+    public static void main(String[] args) throws IOException {
         String response = "inicializada";
-        File ruta = new File("src/");
+        File ruta = new File(System.getProperty("user.dir"));
 
         String[] datosResponseSpace;
-        String[] datosResponseBackslash;
+        // String[] datosResponseBackslash;
 
         // BUCLE DE LA TERMINAL //
         do{
@@ -21,21 +23,31 @@ public class    MiniTerminal {
             switch (datosResponseSpace[0]) {
                 // MOSTRAR DIRECTORIO ACTUAL //
                 case "pwd":
-                    System.out.println(ruta + "/");
-
+                    System.out.println(ruta.getAbsolutePath() + "/");
                     break;
                 // MOVER Y RENOMBRAR ARCHIVOS//
                 case "mv":
-                    File mv1 = new File(datosResponseSpace[1]);
-                    File mv2 = new File(datosResponseSpace[2]);
+                    MiniFileManager.move(ruta, datosResponseSpace);
                     break;
                 // MOVERSE ENTRE DIRECTORIOS //
                 case "cd":
+                    if(datosResponseSpace.length == 2){
+                        ruta = MiniFileManager.changeDir(ruta, datosResponseSpace);
+                    }else if(datosResponseSpace.length > 2){
+                    System.out.println("Demasiados argumentos");
+                    }else{
+                        ruta = new File(System.getProperty("user.dir"));
+                    }
                     break;
+                // CREACIÓN DE DIRECTORIOS // --> Has de cerrar el programa para que los directorios se creen, srry ;P
                 case "mkdir":
-                    MiniFileManager.createDirectory(ruta, datosResponseSpace);
+                    if(datosResponseSpace.length > 1){
+                        MiniFileManager.createDirectory(ruta, datosResponseSpace);
+                    }else {
+                        System.out.println("No has pasado como parámetro de que directorio/s quieres crear");
+                    }
                     break;
-                // VER CONTENIDO DE LA CARPETA ACTUAL//
+                // VER CONTENIDO DE LA CARPETA ACTUAL //
                 case "ls":
                     if (datosResponseSpace.length > 1){
                         MiniFileManager.lOut(datosResponseSpace, ruta, datosResponseSpace[0]);
@@ -51,10 +63,17 @@ public class    MiniTerminal {
                         MiniFileManager.printList(ruta, true);
                     }
                     break;
+                // LIMPIAR TERMINAL //
+                case "clear":
+                    // Estuve mirando de implementarlo, pero la consola de intellj no es una terminal real, los métodos que implemente no funcionaban
+                    // Así que use esta cutrada XD
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                    break;
+                // SALIR DEL BUCLE DE TERMINAL //
                 case "exit":
                     break;
                 default:
-                    System.out.println(response +": command not found");
+                    System.out.println(response +": comando no encontrado");
                     break;
             }
 
