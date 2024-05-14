6c08c323cc7b087297e15fe0e5e87e8f21968005
Josito <josalbarr@alu.edu.gva.es>
2024-05-14 20:35:35 +0200

    acabar ejercicios de escritura

--- Java/File/LecturaEscritura/ex1.java
+++ Java/File/LecturaEscritura/ex1.java
@@ -1,4 +1,24 @@
 package Programacion.Java.File.LecturaEscritura;
 
+import java.io.*;
+
 public class ex1 {
+    public static void main(String[] args) throws IOException {
+        int maxValue = 0;
+        File ruta = new File("src/Programacion/Java/File/LecturaEscritura/Documentos/numeros.txt");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                int lineToInt = Integer.parseInt(line);
+
+                if(lineToInt > maxValue){
+                    maxValue = lineToInt;
+                }
+            }
+        }
+
+        System.out.println("El máximo valor encontrado en la ruta:\n"+ruta+"\n\nEs: "+maxValue);
+
+    }
 }
--- Java/File/LecturaEscritura/ex2.java
+++ Java/File/LecturaEscritura/ex2.java
@@ -1,4 +1,11 @@
 package Programacion.Java.File.LecturaEscritura;
 
+import java.io.File;
+
 public class ex2 {
+    public static void main(String[] args) {
+        File ruta = new File("src/Programacion/Java/File/LecturaEscritura/Documentos/alumnos_notas.txt");
+        String[] alumnoValues;
+
+    }
 }
