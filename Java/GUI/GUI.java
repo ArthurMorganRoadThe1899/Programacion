5c37507c96c8d539f04f444add91733ef71b5278
Joselito <josalbarr@alu.edu.gva.es>
2024-05-21 22:04:24 +0200

    xd

--- Java/GUI/GUI.java
+++ Java/GUI/GUI.java
@@ -3,29 +3,42 @@ package Programacion.Java.GUI;
 import javax.swing.*;
 import java.awt.*;
 
-public class GUI {
+public class GUI extends JPanel {
+    // Nombre de la ventana
+    JFrame frame = new JFrame("HOLA TUTIMUTI, ME SALUDAS??!!??!!?!");
+    ImageIcon image;
+    JLabel campoImagen;
 
-    public static void main(String[] args) {
+    public GUI(){
 
-
-        for(int i = 0; i < 26; i++){
+            // Variables del posicionamiento random y de imagen
             int x = (int) (Math.random()*1920+1);
             int y = (int) (Math.random()*1080+1);
-            JFrame frame = new JFrame("HOLA TUTIMUTI, ME SALUDAS??!!??!!?!");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(90,300);
-            frame.setContentPane(new Image());
+            image = new ImageIcon(getClass().getResource("s.png"));
+            campoImagen = new JLabel(image);
+
+            // Especificaciones
+            frame.add(campoImagen);
+            //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(190,300);
+            //frame.setContentPane(new Imagea());
             frame.setLocation(x, y);
             frame.setVisible(true);
-        }
+
     }
 
-    public class Image extends GUI{
+    public static void main(String[] args) {
+        for(int i = 0; i < 56; i++) {
+            GUI g = new GUI();
+        }
+
+    }
+
+    /*public class Image extends JPanel {
         public void ElementosGraficos(Graphics g){
             ImageIcon fondo = new ImageIcon("https://images.nightcafe.studio/jobs/d5Y1mDQ8YKCe173wWrGz/d5Y1mDQ8YKCe173wWrGz--1--2tfvm.jpg?tr=w-1600,c-at_max");
             g.drawImage(fondo.getImage(), 300,300, 90, 30,null);
         }
 
-    }
-
+    }*/
 }
--- Java/GUI/Imagea.java
+++ Java/GUI/Imagea.java
@@ -1,4 +1,18 @@
 package Programacion.Java.GUI;
 
-public class Imagea {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Imagea extends JPanel {
+    public void ElementosGraficos(Graphics g){
+        ImageIcon fondo = new ImageIcon("s.png");
+        g.drawImage(fondo.getImage(), 300,300, 44, 24,null);
+    }
+
 }
+
+// https://images.nightcafe.studio/jobs/d5Y1mDQ8YKCe173wWrGz/d5Y1mDQ8YKCe173wWrGz--1--2tfvm.jpg?tr=w-1600,c-at_max
