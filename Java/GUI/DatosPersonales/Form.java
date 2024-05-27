4ca1ee48a98b635accada92c6abb475952664aaa
Josito <josalbarr@alu.edu.gva.es>
2024-05-27 18:23:33 +0200

    sw

--- Java/GUI/DatosPersonales/Form.java
+++ Java/GUI/DatosPersonales/Form.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Form extends JPanel {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ventana con botón");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);voy
         frame.setSize(500,500);;
         // ! Centrar
         // frame.setLocation(); //
