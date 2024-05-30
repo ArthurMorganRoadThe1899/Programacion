9d057d09192ed715f3c59004336d8a564b93bc29
Josito <josalbarr@alu.edu.gva.es>
2024-05-30 19:08:19 +0200

    i ending this shit

--- Java/GUI/matematico/Calculadora.java
+++ Java/GUI/matematico/Calculadora.java
@@ -3,8 +3,14 @@ package Programacion.Java.GUI.matematico;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class Calculadora extends JFrame implements ActionListener {
+
+    static JTextField panel = new JTextField(16);
+
+    static ArrayList<String> panelContent = new ArrayList<String>();
+
 
     public Calculadora(){
         FlowLayout flow = new FlowLayout(FlowLayout.CENTER,3,3);
@@ -22,7 +28,6 @@ public class Calculadora extends JFrame implements ActionListener {
 
     // Añadir botones y el panel a la interfaz //
     public void setButtons(){
-        JTextField panel = new JTextField(16);
         JButton B1 = new JButton("1");
         JButton B2 = new JButton("2");
         JButton B3 = new JButton("3");
@@ -40,6 +45,24 @@ public class Calculadora extends JFrame implements ActionListener {
         JButton BMUL = new JButton("*");
         JButton BDEL = new JButton("C");
         JButton BP = new JButton(".");
+
+        B1.addActionListener(this);
+        B2.addActionListener(this);
+        B3.addActionListener(this);
+        B4.addActionListener(this);
+        B5.addActionListener(this);
+        B6.addActionListener(this);
+        B7.addActionListener(this);
+        B8.addActionListener(this);
+        B9.addActionListener(this);
+        B0.addActionListener(this);
+        BEQU.addActionListener(this);
+        BSUM.addActionListener(this);
+        BRES.addActionListener(this);
+        BDIV.addActionListener(this);
+        BMUL.addActionListener(this);
+        BDEL.addActionListener(this);
+        BP.addActionListener(this);
 
         add(panel);
         add(B1);
@@ -63,8 +86,8 @@ public class Calculadora extends JFrame implements ActionListener {
 
     // Iniciar la GUI //
     public void iniciarGUI(){
-        setTitle("Calculadora 4K");
-        setSize(470,600);
+        setTitle("Calculadora 4K ");
+        setSize(200,205);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLocationRelativeTo(null);
@@ -74,6 +97,40 @@ public class Calculadora extends JFrame implements ActionListener {
     // Evento para añadir acciones //
     @Override
     public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        String contentParser = "";
 
+            switch (action) {
+                default:
+                    panelContent.add(action);
+                    contentParser = operacionInterna();
+                    panel.setText(contentParser);
+
+                    break;
+                case "=":
+
+                    break;
+                case "C":
+                    panelContent.removeAll(panelContent);
+                    panel.setText("");
+                    break;
+
+        }
+
+    }
+
+    // Operacion interna //
+    public String operacionInterna(){
+        String e = "";
+        for(int i = 0; i < panelContent.size(); i++){
+            e = e+panelContent.get(i);
+        }
+
+        return e;
+    }
+
+    // Sacar resultado //
+    public void SacarResultado(){
+        
     }
 }
